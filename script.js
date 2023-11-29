8f9e63b1064429088a3aa11c4343d23d71cd4754
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-29 09:40:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const input = document.querySelectorAll(".code");
+for(let i = 0; i < input.length; i++){
+	input[i].addEventListener('keyup', onFocus);
+}
+
+function onFocus(event){
+	if(event.keyCode === 8){
+		event.target.value = "";
+		if(event.target.previousElementSibling)
+		event.target.previousElementSibling.focus();
+	}
+	else{
+		// console.log(event.target);
+		if(event.target.nextElementSibling)
+		event.target.nextElementSibling.focus();
+	}
+}
